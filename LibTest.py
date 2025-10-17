4a8800b9699637d053114ae91e01030f931aa178
lucakoecher-ltts <luca.koecher@ltts.com>
2025-10-17 09:35:28 +0200

    refined LibTest.py

--- LibTest.py
+++ LibTest.py
@@ -3,7 +3,11 @@ import numpy as np
 
 def testFunction():
     print("Hello")
-    a = np.random.rand(3,2)
+    a = np.random.rand(2,3)
     b = np.random.rand(3,2)
     c = a.dot(b)
     print(c)
+
+
+if __name__ == "__main__":
+    testFunction()
